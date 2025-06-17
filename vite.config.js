396240f3f6d22e34970fd92054f61b7eb4d7e8a5
didrikjensen <142822246+didrikjensen@users.noreply.github.com>
2025-06-17 15:27:59 +0200

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Viktig: Endre 'min-portefolje' til navnet på GitHub-repoet ditt!
+  // Hvis repoet ditt heter 'ditt-brukernavn.github.io', skal base være '/'
+  base: '/didrikjensen.gihub.io/',
 })
